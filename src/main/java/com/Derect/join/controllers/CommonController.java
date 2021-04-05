25dc2667de196e2ed6034f675b1a44b8d3151107
Tima Tihinya <tihinia.timofei@outlook.com>
2021-04-05 12:28:14 +0300

    password confrim v9.6

--- src/main/java/com/Derect/join/controllers/CommonController.java
+++ src/main/java/com/Derect/join/controllers/CommonController.java
@@ -1,13 +1,22 @@
 package com.Derect.join.controllers;
 
+import com.Derect.join.entity.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CommonController {
 
     @GetMapping
-    public String homPage(){
+    public String homPage(@AuthenticationPrincipal User user,
+                          Model model) {
+        if(user != null) {
+            model.addAttribute("name", user.getUsername());
+        } else{
+            model.addAttribute("name", "Guest");
+        }
         return "homePage";
     }
 }
--- src/main/java/com/Derect/join/controllers/RegistrationController.java
+++ src/main/java/com/Derect/join/controllers/RegistrationController.java
@@ -6,6 +6,7 @@ import com.Derect.join.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,14 +25,24 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Model model) {
+    public String addUser(
+            @RequestParam("password2") String password2,
+            User user, Model model) {
         User userFromDb = userRepo.findByUsername(user.getUsername());
+
+        boolean isConfirmEmpty = StringUtils.isEmpty(password2);
+
+        if(isConfirmEmpty){
+            return "registration";
+        }
 
         if (userFromDb != null) {
             model.addAttribute("message", "User exists!");
             return "registration";
         }
-
+        if(!user.getPassword().equals(password2) && user.getPassword() != null){
+            return "registration";
+        }
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
         userRepo.save(user);
