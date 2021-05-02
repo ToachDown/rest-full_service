df369894e398e4ecf3a882ad9439053dbb623cf2
Tima Tihinya <tihinia.timofei@outlook.com>
2021-05-02 20:58:23 +0300

    pre-update role  v11.3

--- src/main/java/com/Derect/join/controllers/UserController.java
+++ src/main/java/com/Derect/join/controllers/UserController.java
@@ -1,14 +1,18 @@
 package com.Derect.join.controllers;
 
+import com.Derect.join.entity.Role;
 import com.Derect.join.entity.User;
 import com.Derect.join.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
@@ -23,5 +27,23 @@ public class UserController {
         model.addAttribute("isAuthorized",user);
         model.addAttribute("usersList", userRepository.findAll());
         return "userList";
+    }
+
+    @GetMapping("{user}")
+    public String userEditRole(@PathVariable User user,
+                               Model model){
+        model.addAttribute("roles", Role.values());
+        model.addAttribute("USER",user);
+        model.addAttribute("isAuthorized",user);
+        return "userEdit";
+    }
+
+    @PostMapping
+    public String userSave(@RequestParam("userID") User user,
+                           @RequestParam String username){
+        user.setUsername(username);
+
+        userRepository.save(user);
+        return "redirect:/user";
     }
 }
