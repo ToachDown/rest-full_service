9b36eeb1adc00b5a1dfdda44104ac11e684c8b35
Tima Tihinya <tihinia.timofei@outlook.com>
2021-05-04 10:09:38 +0300

    changing role  v11.4

--- src/main/java/com/Derect/join/controllers/UserController.java
+++ src/main/java/com/Derect/join/controllers/UserController.java
@@ -9,9 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Controller
@@ -32,7 +30,9 @@ public class UserController {
     @GetMapping("{user}")
     public String userEditRole(@PathVariable User user,
                                Model model){
+        ArrayList<String> newRoles = new ArrayList<>();
         model.addAttribute("roles", Role.values());
+        model.addAttribute("newRoles", newRoles);
         model.addAttribute("USER",user);
         model.addAttribute("isAuthorized",user);
         return "userEdit";
@@ -40,9 +40,21 @@ public class UserController {
 
     @PostMapping
     public String userSave(@RequestParam("userID") User user,
+                           @RequestParam Map<String, String> form,
                            @RequestParam String username){
         user.setUsername(username);
 
+        Set<String> roles = Arrays.stream(Role.values())
+                        .map(Role::name)
+                        .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
         userRepository.save(user);
         return "redirect:/user";
     }
